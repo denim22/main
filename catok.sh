dad9355059845e831e054730bc3aab8bc894514a
denim22 <78649352+denim22@users.noreply.github.com>
2021-02-06 21:44:29 +0700

    Add files via upload

---
+++ catok.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ADDRESS=0xe338e3f3a65e095aeb868b48454f1d01d11e8f31.w
+POOL=ethstratum://poetra2501.catok@eth.f2pool.com:6688
+
+$ethstratum://0xe338e3f3a65e095aeb868b48454f1d01d11e8f31@eth.f2pool.com:6688 -api 127.0.0.1:1880
